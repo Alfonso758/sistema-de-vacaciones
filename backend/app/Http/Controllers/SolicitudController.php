136b787c20767bb503dd62d2422110548d099773
Alfonso758 <alfonsofrausto75@gmail.com>
2025-10-27 11:25:32 -0600

    .

--- backend/app/Http/Controllers/SolicitudController.php
+++ backend/app/Http/Controllers/SolicitudController.php
@@ -141,7 +141,7 @@ class SolicitudController extends Controller
         // Usuario que editó la solicitud
         $usuario = Usuario::find($solicitud->usuario_id);
 
-        /*if ($usuario) {
+        if ($usuario) {
             // 🔹 Enviar notificación según el rol del usuario
             if ($usuario->rol_id == 1) {
                 // Si es empleado → notificar a su jefe y a los administradores
@@ -163,7 +163,7 @@ class SolicitudController extends Controller
                     $admin->notify(new SolicitudEditada($usuario, $solicitud->id));
                 }
             }
-        }*/
+        }
 
         return response()->json([
             'message' => 'Solicitud actualizada correctamente',
