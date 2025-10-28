1d1b107e5c5e47397a57f0ac2a7e268e88ca66cc
Alfonso758 <alfonsofrausto75@gmail.com>
2025-10-28 11:09:41 -0600

    correccion en la notificacion al enviar soicitud empleados y jefes

--- backend/app/Notifications/NuevaSolicitud.php
+++ backend/app/Notifications/NuevaSolicitud.php
@@ -19,26 +19,47 @@ class NuevaSolicitud extends Notification
 
     public function via($notifiable)
     {
-        // Registrar la notificación interna en la base de datos
-        Notificacion::create([
-            'id_usuario'  => $notifiable->id,
-            'titulo'      => 'Nueva solicitud de vacaciones',
-            'mensaje'     => "El usuario {$this->usuario->name} ha realizado una nueva solicitud de vacaciones.",
-            'leido'       => 2,
-            'fecha_envio' => now(),
-        ]);
+        if ($this->usuario->rol_id === 2) {
+            Notificacion::create([
+                'id_usuario'  => $notifiable->id,
+                'titulo'      => 'Nueva solicitud de vacaciones',
+                'mensaje'     => "El usuario {$this->usuario->name} ha realizado una nueva solicitud de vacaciones. Puedes rechazar la solicitud dentro de las 72 horas.",
+                'leido'       => 2,
+                'fecha_envio' => now(),
+            ]);
 
-        return ['mail'];
+            return ['mail'];
+        } else {
+            Notificacion::create([
+                'id_usuario'  => $notifiable->id,
+                'titulo'      => 'Nueva solicitud de vacaciones',
+                'mensaje'     => "El usuario {$this->usuario->name} ha realizado una nueva solicitud de vacaciones.",
+                'leido'       => 2,
+                'fecha_envio' => now(),
+            ]);
+
+            return ['mail'];
+        }
     }
 
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-            ->subject('Nueva solicitud de vacaciones')
-            ->greeting('Hola ' . $notifiable->name . ',')
-            ->line("El usuario {$this->usuario->name} {$this->usuario->surnames} ha creado una nueva solicitud de vacaciones.")
-            ->line('Por favor revisa la solicitud y procede con la aprobación o rechazo correspondiente.')
-            ->action('Ver solicitud', 'https://vacaciones.sokodev.com')
-            ->line('Gracias por dar seguimiento a las solicitudes de tus empleados.');
+        if ($this->usuario->rol_id === 2) {
+            return (new MailMessage)
+                ->subject('Nueva solicitud de vacaciones')
+                ->greeting('Hola ' . $notifiable->name . ',')
+                ->line("El usuario {$this->usuario->name} {$this->usuario->surnames} ha creado una nueva solicitud de vacaciones.")
+                ->line('Puedes rechazar la solicitud dentro de las 72 horas.')
+                ->action('Ver solicitud', 'https://vacaciones.sokodev.com')
+                ->line('Gracias por dar seguimiento a las solicitudes de tus empleados.');
+        } else {
+            return (new MailMessage)
+                ->subject('Nueva solicitud de vacaciones')
+                ->greeting('Hola ' . $notifiable->name . ',')
+                ->line("El usuario {$this->usuario->name} {$this->usuario->surnames} ha creado una nueva solicitud de vacaciones.")
+                ->line('Por favor revisa la solicitud y procede con la aprobación o rechazo correspondiente.')
+                ->action('Ver solicitud', 'https://vacaciones.sokodev.com')
+                ->line('Gracias por dar seguimiento a las solicitudes de tus empleados.');
+        }
     }
 }
